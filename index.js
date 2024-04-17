4b3a8e229ddcd27f9d16e1d0c8cd146cbda65ca8
kenkipkoec <itsmetrokenaki@gmal.com>
2024-04-17 16:47:55 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Your Name is the champion'; 
+
+document.body.appendChild(newHeader);
